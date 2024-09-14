334e4ac1f267a557ad8850f7548b20c7606bc5cc
fengyuanhua <fengyuanhua@cubesec.cn>
2024-09-14 17:21:26 +0800

    feat[log]添加日志

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.2
 require (
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/mingrammer/commonregex v1.0.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/viper v1.19.0
 	golang.org/x/net v0.26.0
 )
 
@@ -14,16 +16,31 @@ require (
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.2.4 // indirect
 	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- utils/conf/config.go
+++ utils/conf/config.go
@@ -1,1 +1,110 @@
-package conf
+package config
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+var Settings = &SettingsConf{}
+
+type SettingsConf struct {
+	App App `mapstructure:"app"`
+}
+
+type App struct {
+	Secret string `mapstructure:"jwt_secret_token"`
+	Salt   string `mapstructure:"salt"`
+}
+
+// Provider defines a set of read-only methods for accessing the application
+// configuration params as defined in one of the config files.
+type Provider interface {
+	ConfigFileUsed() string
+	Get(key string) interface{}
+	GetBool(key string) bool
+	GetDuration(key string) time.Duration
+	GetFloat64(key string) float64
+	GetInt(key string) int
+	GetInt64(key string) int64
+	GetSizeInBytes(key string) uint
+	GetString(key string) string
+	GetStringMap(key string) map[string]interface{}
+	GetStringMapString(key string) map[string]string
+	GetStringMapStringSlice(key string) map[string][]string
+	GetStringSlice(key string) []string
+	GetTime(key string) time.Time
+	InConfig(key string) bool
+	IsSet(key string) bool
+}
+
+var defaultConfig *viper.Viper
+var provider *viper.Viper
+var once sync.Once
+var onceRedis sync.Once
+
+// Config returns a default config providers
+func Config() Provider {
+	return defaultConfig
+}
+
+// LoadConfigProvider returns a configured viper instance
+func LoadConfigProvider() Provider {
+
+	once.Do(func() {
+		provider = readViperConfig()
+	})
+	return provider
+}
+
+func LoadConfigProviderRedis() Provider {
+	onceRedis.Do(func() {
+		provider = readViperConfig()
+	})
+	return provider
+}
+
+func init() {
+	defaultConfig = readViperConfig()
+}
+
+func readViperConfig() *viper.Viper {
+	// 环境变量设置支持
+	v := viper.New()
+	v.SetEnvPrefix("op")
+
+	// 文件设置支持
+	v.AddConfigPath(".")
+	v.AddConfigPath("/srv/op_dev")
+	v.SetConfigName("settings")
+	v.SetConfigType("toml")
+
+	v.AutomaticEnv()
+	replacer := strings.NewReplacer(".", "_")
+	v.SetEnvKeyReplacer(replacer)
+
+	err := v.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("ratal error config file: %s ", err))
+	}
+
+	// global defaults
+	v.SetDefault("json_logs", false)
+	v.SetDefault("loglevel", "debug")
+
+	_ = v.Unmarshal(Settings)
+
+	// 本地开发配置
+	runLevel := v.GetString("run_level")
+	if runLevel == "development" {
+		v.SetConfigName("settings.local")
+		v.SetConfigType("toml")
+		v.AddConfigPath(".")
+		v.MergeInConfig()
+	}
+
+	return v
+}
