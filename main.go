e6530d40d28369b9801cd2df4569af95955a0ef4
fengyuanhua <fengyuanhua@cubesec.cn>
2024-09-18 11:18:28 +0800

    feat[git]添加gitignore

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	config "miniScan/utils/conf"
-	"miniScan/utils/log"
+	log "miniScan/utils/log"
 )
 
 func main() {
-	cfg := config.LoadConfigProvider()
-	logger := log.NewLogger(cfg)
-	logger.Error("---------------------")
+	// 初始化配置
+	cfg := config.LoadConfigProvider() // 加载配置文件
+
+	// 记录端口扫描日志
+	logP := log.LogPortScan
+	// 记录子域名扫描的警告日志
+	logD := log.LogDomain
+	// 记录 web 探测相关的信息
+	logW := log.LogWebInfo
+	logD.Info(cfg.GetBool(""))
+	logP.Warn(cfg.GetBool(""))
+	logW.Error(cfg.GetBool(""))
 }
